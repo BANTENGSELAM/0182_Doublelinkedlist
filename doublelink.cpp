9ac31d6410593257701390ed09e3ae0a3fd6d0be
“BANTENGSELAM” <pedjuangjava@gmail.com>
2026-05-01 15:30:35 +0700

    step 8: Locate position for insertion

--- doublelink.cpp
+++ doublelink.cpp
@@ -56,5 +56,19 @@ class DoubleLinkedList
         return;
         }
 
+        //insert in between node
+        //step 8: Locate position for insertion
+        Node *current = START;
+        while (current->next != NULL && current->next->noMhs < nim)
+        {
+            current = current->next;
+        }
+
+        if (current->next != NULL && nim == current->next->noMhs)
+        {
+            cout << "\nDuplicate roll number not allowed" << endl;
+            return;
+        }
+
     }
 };
